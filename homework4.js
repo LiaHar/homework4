241349f7269347e9b9bc74b078d7ba31f45ee76b
LiaHar <lia-ht@yandex.ru>
2018-10-06 20:11:06 +0400

    second v.

--- homework4.js
+++ homework4.js
@@ -1,35 +1,4 @@
-
-//RIGHT STAR TRIANGLE REVERESED
-
-const func = function(a, count) {
-    let str = "";
-    while(count > 0) {
-      str = str + a;
-      count = count - 1;
-    }
-    return str;
-  }
-  
-  const func2 = function(a, s){
-    console.log( func("*", s));
-    
-  }
-  
-  const f1 = function(a) {
-    let k = (2 * a - 1);
-    
-    while(0 < a) {
-      func2(a, k);
-      a = a -1;
-      k = k -2;
-    }
-  }
-  f1(5);
-
-
-
-
-//STAR TRIANGLE REVERESED
+//1.STAR TRIANGLE REVERESED
 const starConcut = function(height, spaceCountNumber) {
     let str = "";
     while(spaceCountNumber > 0) {
@@ -57,45 +26,7 @@ const starConcut = function(height, spaceCountNumber) {
   }
   makeTriangle(5);
 
-
-
-
-  
-
-//STAR TRIANGLE NOT REVERESED
-const f3 = function(a, count) {
-    let st = "";
-    while(count > 0) {
-      st = st + a;
-      count = count - 1;
-    }
-    return st;
-  }
-  
-  
-  
-  const f2 = function(a, starCount){
-    console.log(f3(" ",a-1) + f3("*", starCount));
-  }
-  
-  
-  
-  const f1 = function(a) {
-    let k = 1;
-    while(0 < a){
-      f2(a,k)
-      a= a - 1;
-      k = k + 2;
-    
-    }
-  }
-  f1(5)
-  
-
-
-
-
-//FIND MAX MIN
+//2.FIND MAX MIN
   const findMinMax = function findMinMax(arr, bool) {
     let maxmin = arr[0];
     let inx = 1;
@@ -124,52 +55,10 @@ const f3 = function(a, count) {
    
    console.log(findMinMax([4, 2, 66, -44, 8], true));
    console.log(findMinMax([4, 2, 66, -44, 8], false));
-   
-   
 
-
-
-
-// STAR TRIANGLE REVERSED OLD VERSION
-  const func = function(a, count) {
-    let str = "";
-    while(count > 0) {
-      str = str + a;
-      count = count - 1;
-    }
-    return str;
-  }
-  
-  
-  
-  
-  const func2 = function(r, s){
-    console.log( func(" ", r ) + func("*", s));
-    
-  }
-  
-  
-  
-  const f1 = function(a) {
-    let k = 2 * a - 1;
-    let r = 0;
-    while(0 < a) {
-      
-      func2(r, k);
-      a = a -1;
-      r = r + 1;
-      k = k -2;
-    }
-  }
-  f1(5);
-  
-  
-
-  
-// MULTITOSINGLE ONE ARRAY
+// 3.MULTITOSINGLE ONE ARRAY
   const multiToSingle = function(arr) {
     let i = 0;
-    let k = 0;
     let newarr = [];
     while(i < arr.length){
       let k = 0;
@@ -184,16 +73,12 @@ const f3 = function(a, count) {
   }
   console.log(multiToSingle([[1, 2, 3], [4, 5, 6]]));
   
-
-
-
-  
-// FOREACH RIGHT VERSION
-const forEach = function forEach(arr, func){
-    let i = 0;
+//4.FOREACH RIGHT VERSION
+const forEach = function (arr, func){
+      let i = 0;
       
       while(i < arr.length ){
-        let b = func(arr[i]);
+        func(arr[i]);
         i = i + 1;
       }
       
@@ -205,15 +90,7 @@ const forEach = function forEach(arr, func){
     
     });
    
-  
-
-
-
-
-
-
-
-// SUM OF THE ARRAY CHARS.
+//5.SUM OF THE ARRAY CHARS.
     const sum = function sum(arr){
         let getSum = 0;
         let inx = 0;
@@ -226,13 +103,7 @@ const forEach = function forEach(arr, func){
       
       console.log( sum([4, 3, 2]));
 
-
-
-
-
-
-
-      // REVERSE STRING
+ //6. REVERSE STRING
       const reverse = function reverse(str){
         let inx= str.length - 1;
        let newStr = "";
@@ -246,15 +117,7 @@ const forEach = function forEach(arr, func){
      }
      console.log(reverse('abcd'));
 
-
-
-
-
-
-
-
-
-// CHECKERBOARD RIGHT VERSION 
+//7.CHECKERBOARD RIGHT VERSION 
 const func = function(a, count){
     let str = "*";
     while(count > 0){
